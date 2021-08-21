3f0f568afa59637413160cbaa3f013b062c947f2
Ineta Bliudziute <81913593+inetabliu@users.noreply.github.com>
2021-08-21 16:11:47 +0100

    Change toast position

--- client/src/components/BuyToast.js
+++ client/src/components/BuyToast.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Row, Col, Toast, Button } from 'react-bootstrap'
+import { Row, Col, Toast, Button, ToastContainer } from 'react-bootstrap'
 
 const BuyToast = () => {
   const [show, setShow] = useState(false)
@@ -7,16 +7,19 @@ const BuyToast = () => {
   return (
 
     <Row>
-      <Col xs={6}>
-        <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+
+      <ToastContainer position="top-end" className="p-3">
+        <Toast position="top-end" onClose={() => setShow(false)} show={show} delay={3000} autohide>
           <Toast.Header>
             <strong className="me-auto">Success</strong>
           </Toast.Header>
           <Toast.Body>Your item has been sucessfully added to your basket</Toast.Body>
         </Toast>
-      </Col>
+      </ToastContainer>
+
       <Col>
         <Button onClick={() => setShow(true)} style={{ width: '100%', marginBottom: '2.5%', marginTop: '2.5%' }} className="btn btn-primary btn-lg" variant="danger">Buy me</Button>
+
       </Col>
     </Row>
   )
